39704bc3f75478a5672dff0d45dd873f1da1fe53
sicily621 <zhangm_2014@163.com>
2022-05-18 15:33:54 +0800

    实现 解析element 功能

--- src/runtime-core/src/parse.ts
+++ src/runtime-core/src/parse.ts
@@ -1,14 +1,43 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType{
+    Start,
+    End
+}
+
 export function baseParse(content:string){
     const context = createParserContext(content);
     return createRoot(parseChildren(context));
 }
 function parseChildren(context){
     const nodes:any = [];
-    const node = parseInterpolation(context);
+    let node;
+    const s = context.source;
+    if(s.startsWith("{{")){
+        node = parseInterpolation(context);
+    }else if(s[0]==='<'){
+        if(/[a-z]/i.test(s[1])){
+            node = parseElement(context);
+        }
+    }
     nodes.push(node);
     return nodes;
+}
+function parseElement(context:any){
+    const element = parseTag(context,TagType.Start);
+    parseTag(context,TagType.End);
+    return element;
+}
+function parseTag(context:any,type:TagType){
+    const match:any = /^<\/?([a-z]*)/i.exec(context.source);
+    const tag = match[1];
+    advanceBy(context,match[0].length);
+    advanceBy(context,1);
+    if(type === TagType.End) return;
+    return {
+        type:NodeTypes.ELEMENT,
+        tag
+    }
 }
 function parseInterpolation(context){
     const openDelimiter = "{{";
--- src/runtime-core/tests/parse.spec.ts
+++ src/runtime-core/tests/parse.spec.ts
@@ -14,3 +14,12 @@ describe("Parse",()=>{
         })
     })
 })
+describe("element",()=>{
+    it("simple element div",()=>{
+        const ast = baseParse("<div></div>");
+        expect(ast.children[0]).toStrictEqual({
+            type:NodeTypes.ELEMENT,
+            tag:"div"
+        })
+    })
+})
